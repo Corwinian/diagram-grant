99de3968b0f4f215e9efc84c5315831acd433c9f
Corwin <DartRavern@gmail.com>
2011-12-10 00:55:21 +1100

    добавил в репозиторий скрипт для заполения данными используемые таблицы для тестирования

---
+++ values.sql
@@ -0,0 +1,15 @@
+
+insert into Company (id,name) values (1, "Google");
+insert into Company (id,name) values (2, "Motorola");
+insert into Company (id,name) values (3, "Apple");
+insert into Company (id,name) values (4, "mic");
+
+
+insert into Developer (login, pass, name, isAdmin, company) values ("sss", "sdsds", "sss", true, 1);
+insert into Developer (login, pass, name, company) values ("west", "sdfs", "sss", true,1);
+insert into Developer (login, pass, name, company) values("wersr", "sdsds", "sss", 1);
+insert into Developer (login, pass, name, company) values("ing", "sdsds", "sss", 2);
+insert into Developer (login, pass, name, company) values("sqwert", "sdsds", "sss", 2);
+insert into Developer (login, pass, name, company) values("qwscfs", "sdsds", "sss", 2);
+insert into Developer (login, pass, name, company) values("weddfd", "sdsds", "sss", 2);
+insert into Developer (login, pass, name, company) values("wertdss", "sdsds", "sss", 3);
